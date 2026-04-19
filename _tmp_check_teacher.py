e7e8e3fd640f117d694a09809e52e79c5c2fe1c8
Jonibek Jorayev <jonibekjorayev733@gmail.com>
2026-04-19 09:28:16 +0500

    security: remove sensitive files, pycache, temp scripts and embedded repos from tracking

--- _tmp_check_teacher.py
+++
@@ -1,9 +0,0 @@
-from backend.database import SessionLocal
-from backend.models import Teacher
-
-db = SessionLocal()
-rows = db.query(Teacher).all()
-for t in rows:
-    p = t.password or ''
-    print(t.id, t.email, len(p), 'has_dollar' if '$' in p else 'no_dollar', p[:20])
-db.close()
--- _tmp_check_users.py
+++
@@ -1,7 +0,0 @@
-from backend.database import SessionLocal
-from backend.models import Admin, Student
-
-db = SessionLocal()
-print('admins:', [(a.id, a.email) for a in db.query(Admin).all()])
-print('students:', [(s.id, s.email) for s in db.query(Student).all()])
-db.close()
